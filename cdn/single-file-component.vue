22b3e8dd812a6d47e78feceb961ceb2ce2d85e48
Sunha Ryu <rainofpainki@naver.com>
2023-03-31 14:34:51 +0900

    단일 파일 컴포넌트

---
+++ cdn/single-file-component.vue
@@ -0,0 +1,21 @@
+<template><!-- 컴포넌트 정보 표시 -->
+  <div class="hello">
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+/* ES2015 Export 기능 */
+export default {
+  name: 'hello',
+  data() {
+    return {
+      msg: 'Vue.js 앱에 오신 걸 환영합니다'
+    }
+  }
+}
+</script>
+
+<!-- CSS 범위 지정을 위해 해당 컴포넌트에만 "scoped" 속성을 추가 -->
+<style scoped>
+</style>
