f8e1ddf8b0f0841b1599b06f78d0b7143d60c7f5
Sunha Ryu <rainofpainki@naver.com>
2023-04-02 11:53:23 +0900

    컴포넌트 추가 및 라우팅 설정

--- petstore/src/router/index.js
+++ petstore/src/router/index.js
@@ -1,15 +1,28 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vue from "vue";
+import Router from "vue-router";
+import Form from "@/component/Form";
+import Form from "@/component/Main";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
+  mode: "history", // 경로에 해시가 없는 히스토리 모드
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: "/",
+      name: "Main",
+      component: Main,
+      props: true
+    },
+    {
+      path: "/form",
+      name: "Form",
+      component: Form,
+      props: true
+    },
+    {
+      path: "*",
+      redirect: "/"
     }
   ]
-})
+});
